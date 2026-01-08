f1ac0be62d21f1a5360fffd54e170e49d12bffe2
anilbegin <98578895+anilbegin@users.noreply.github.com>
2026-01-08 19:14:53 +0530

    passing setLoggedIn props basic

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,10 +1,13 @@
-import React from "react"
+import React, {useState} from "react"
 import { Link } from "react-router-dom"
 
 // my components
 import HeaderLoggedout from "./HeaderLoggedout"
+import HeaderLoggedIn from "./HeaderLoggedIn"
 
 function Header() {
+  const [loggedIn, setLoggedIn] = useState()
+
   return (
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
@@ -13,7 +16,7 @@ function Header() {
             ComplexApp
           </Link>
         </h4>
-        <HeaderLoggedout />
+        {loggedIn ? <HeaderLoggedIn setLoggedIn={setLoggedIn} /> : <HeaderLoggedout setLoggedIn={setLoggedIn} />}
       </div>
     </header>
   )
