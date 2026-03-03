e94044ba9e7293f754f3b0fd8f67bec363769879
anilbegin <98578895+anilbegin@users.noreply.github.com>
2026-03-03 19:52:23 +0530

    Search function p1

--- app/Main.js
+++ app/Main.js
@@ -18,6 +18,7 @@ import FlashMessages from './components/FlashMessages'
 import Profile from './components/Profile'
 import EditPost from './components/EditPost'
 import NotFound from './components/NotFound'
+import Search from './components/Search'
 
 import StateContext from './StateContext'
 import DispatchContext from './DispatchContext'
@@ -30,7 +31,8 @@ function Main() {
       token: localStorage.getItem('complexAppToken'),
       username: localStorage.getItem('complexAppUsername'),
       avatar: localStorage.getItem('complexAppAvatar')
-    }
+    },
+    isSearchOpen: false
   }
 
   function ourReducer(draft, action) {
@@ -44,7 +46,13 @@ function Main() {
         return 
       case 'flashMessage' :
         draft.flashMessages.push(action.value)
-        return   
+        return
+      case 'openSearch' :
+        draft.isSearchOpen = true
+        return
+      case 'closeSearch' :
+        draft.isSearchOpen = false
+        return       
     } 
   }
   const [state, dispatch]= useImmerReducer(ourReducer, initialState)
@@ -78,6 +86,7 @@ function Main() {
             <Route path="/terms" element={<Terms />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
+          {state.isSearchOpen ? <Search /> : ''}
           <Footer />
         </BrowserRouter>
       </DispatchContext.Provider>
