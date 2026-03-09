40c73fe45906fc55cb79e90605f54ec87b5b1d91
anilbegin <98578895+anilbegin@users.noreply.github.com>
2026-03-09 13:57:40 +0530

    follow user p1- useImmer instead of useState

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useContext, useState } from "react"
+import React, { useEffect, useContext } from "react"
+import { useImmer } from 'use-immer'
 import { useParams } from "react-router-dom"
 import Axios from 'axios'
 
@@ -9,12 +10,17 @@ import StateContext from "../StateContext"
 function Profile() {
   const {username} = useParams()
   const appState = useContext(StateContext)
-  const [profileData, setProfileData] = useState({
-    profileUsername: '...',
-    profileAvatar: 'https://gravatar.com/avatar/placeholder?s=128',
-    isFollowing: false,
-    counts: {
-      postCount: '', followerCount: '', followingCount: ''
+  const [state, setState] = useImmer({
+    followActionLoading: false,
+    startFollowingRequestCount: 0,
+    stopFollowingRequestCount: 0,
+    profileData : {
+      profileUsername: '...',
+      profileAvatar: 'https://gravatar.com/avatar/placeholder?s=128',
+      isFollowing: false,
+      counts: {
+        postCount: '', followerCount: '', followingCount: ''
+      }
     }
   })
 
@@ -25,7 +31,9 @@ function Profile() {
           token: appState.user.token
         })
       //  console.log(response.data)
-        setProfileData(response.data)
+        setState(draft => {
+          draft.profileData = response.data
+        })
       } catch (e) {
         console.log('there was problem')
       }
@@ -36,19 +44,19 @@ function Profile() {
   return (
     <Page title='Profile Screen'>
       <h2>
-        <img className="avatar-small" src={ profileData.profileAvatar } /> { profileData.profileUsername }
+        <img className="avatar-small" src={ state.profileData.profileAvatar } /> { state.profileData.profileUsername }
         <button className="btn btn-primary btn-sm ml-2">Follow <i className="fas fa-user-plus"></i></button>
       </h2>
 
       <div className="profile-nav nav nav-tabs pt-2 mb-4">
         <a href="#" className="active nav-item nav-link">
-          Posts: { profileData.counts.postCount }
+          Posts: { state.profileData.counts.postCount }
         </a>
         <a href="#" className="nav-item nav-link">
-          Followers: { profileData.counts.followerCount }
+          Followers: { state.profileData.counts.followerCount }
         </a>
         <a href="#" className="nav-item nav-link">
-          Following: { profileData.counts.followingCount }
+          Following: { state.profileData.counts.followingCount }
         </a>
       </div>
 
