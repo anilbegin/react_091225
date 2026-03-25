c1844db0c87811bed00814e5d0f0e1d002990580
anilbegin <98578895+anilbegin@users.noreply.github.com>
2026-03-25 14:14:56 +0530

    basic chat - Chat p3

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useContext, useRef } from "react"
 import {useImmer} from 'use-immer'
+import io from "socket.io-client"
 import StateContext from "../StateContext"
 import DispatchContext from "../DispatchContext"
+const socket = io("http://localhost:8080") 
+// this will establish an ongoing connection.. 
+//..between the Browser and the backend Server.
 
 function Chat() {
   const chatField = useRef(null)
@@ -25,9 +29,21 @@ function Chat() {
     })
   }
 
+  useEffect(() => {
+    socket.on("chatFromServer", (message) => {
+      setState(draft => {
+        draft.chatMessages.push(message)
+      })
+    })
+  } , [])
+
   function handleSubmit(e) {
     e.preventDefault()
     // send message to Chat Server 
+    socket.emit("chatFromBrowser", {
+      message: state.fieldValue,
+      token: appState.user.token
+    })
     
     setState(draft => {
       // Add message to State Collection of Messages
@@ -70,14 +86,14 @@ function Chat() {
           return (
             <div className="chat-other">
               <a href="#">
-                <img className="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128" />
+                <img className="avatar-tiny" src={message.avatar} />
               </a>
               <div className="chat-message">
                 <div className="chat-message-inner">
                   <a href="#">
-                    <strong>barksalot:</strong>
+                    <strong>{message.username}:</strong>
                   </a>
-                  Hey, I am good, how about you?
+                  {message.message}
                 </div>
               </div>
             </div>
